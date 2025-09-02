2115ee57736402c71eeb82165282ff20b2c42c40
FJanesari <FJanesari@github.com>
2025-09-02 19:49:05 +0330

    fix fanfact section

--- core/views.py
+++ core/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from product.models import Product
-from .models import HomeInfo
+from .models import HomeInfo, FanFact
 
 
 def home(request):
     products = Product.objects.all()
     banner = HomeInfo.objects.filter(is_active=True).first()
-    return render(request, 'home/index.html', {"products": products, "banner": banner})
+    fanfacts = FanFact.objects.filter(is_active=True)
+    return render(request, 'home/index.html', {"products": products, "banner": banner, "fanfacts": fanfacts})
 
 
 def contact(request):
