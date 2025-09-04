1c70d39a80e961eea2c1c5f6b5f6531cbfd51f75
FJanesari <FJanesari@github.com>
2025-09-04 10:47:48 +0330

    fix and edit blog details page

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,14 @@ from django.core.paginator import Paginator
 
 def blog_detail(request, slug):
     article = get_object_or_404(Article, slug=slug)
-    return render(request, "blog-details.html", {"article": article})
+    recent_posts = Article.objects.exclude(id=article.id).order_by("-created_at")[:3]
+    query = request.GET.get("q")
+    if query:
+        recent_posts = Article.objects.filter(title__icontains=query)[:3]
+    return render(request, "blog-details.html", {"article": article,
+                                                 "query": query,
+                                                 "recent_posts": recent_posts,
+                                                 })
 
 
 def blog(request, page=1):
