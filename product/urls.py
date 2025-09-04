712ff90703e34fd87ddec54467f7dd81685b5dee
FJanesari <FJanesari@github.com>
2025-09-04 09:18:40 +0330

    fix linkes in product page

--- product/urls.py
+++ product/urls.py
@@ -6,5 +6,6 @@ app_name = "product"
 
 urlpatterns = [
     path('<slug:slug>/', views.product_detail, name='product_detail'),
-    path('<slug:slug>/', views.product_type, name='product_type'),
+    path("<slug:slug>/page/<int:page>/", views.product_detail, name="product_detail_page"),
+    path('product-type/<slug:slug>/', views.product_type, name='product_type'),
 ]
--- product/views.py
+++ product/views.py
@@ -3,16 +3,16 @@ from .models import Product, ProductType
 from django.core.paginator import Paginator
 
 
-def product_detail(request, slug):
+def product_detail(request, slug, page=1):
     product = get_object_or_404(Product, slug=slug)
     product_type = ProductType.objects.filter(is_active=True)
     paginator = Paginator(product_type, 6)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
+    page_obj = paginator.get_page(page)
     return render(request, "product-details.html", {"product": product,
                                                     "product_type": page_obj,
                                                     })
 
 
-def product_type(request):
-    return render(request, "product-type.html", {"product_type": product_type})
+def product_type(request, slug):
+    type = get_object_or_404(ProductType, slug=slug, is_active=True)
+    return render(request, "product-type.html", {'type': type})
