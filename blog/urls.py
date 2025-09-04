c1c9671eaa617558f23e1af73a82e79f0633d65c
FJanesari <FJanesari@github.com>
2025-09-04 10:19:19 +0330

    fix and edit blog page

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ app_name = "blog"
 urlpatterns = [
     path('blog/', views.blog, name='blog'),
     path('<slug:slug>/', views.blog_detail, name='blog_detail'),
+    path("<slug:slug>/page/<int:page>/", views.blog_detail, name="blog_detail_page"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Article, BlogInfo
+from django.core.paginator import Paginator
 
 
 def blog_detail(request, slug):
@@ -7,10 +8,12 @@ def blog_detail(request, slug):
     return render(request, "blog-details.html", {"article": article})
 
 
-def blog(request):
+def blog(request, page=1):
     article = Article.objects.filter(is_active=True)
     blog_info = get_object_or_404(BlogInfo)
+    paginator = Paginator(article, 6)
+    page_obj = paginator.get_page(page)
     return render(request, "blog.html", {
-        "article": article,
+        "article": page_obj,
         "blog_info": blog_info,
     })
