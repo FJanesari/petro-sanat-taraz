afd9789f2a92e7c0f0bf1d9bc5bd7ed11edab601
FJanesari <FJanesari@github.com>
2025-09-13 16:26:21 +0330

    Edit settings

--- petrosanattaraz/settings.py
+++ petrosanattaraz/settings.py
@@ -164,6 +164,30 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+CORS_ALLOW_CREDENTIALS = True
+
+#CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://127.0.0.1:5500",
+#     "http://localhost:5500",
+#     "http://localhost",
+#     "http://127.0.0.1",
+#     "https://test-seo.petrosanattaraz.com",
+# ]
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://test-seo.petrosanattaraz.com",
+    "http://test-seo.petrosanattaraz.com"
+]
+
+
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
 CKEDITOR_5_UPLOAD_FILE_TYPES = ['jpeg', 'png', 'jpg', 'webp']
 
 CKEDITOR_5_CONFIGS = {
